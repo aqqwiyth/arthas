63dde9046746eafc33dea8e3e19ea7cfe95cd440
欧洲游 <ouzhouyou@raycloud.com>
2020-04-21 18:36:33 +0800

    redefine backup

--- core/src/main/java/com/taobao/arthas/core/command/klass100/ClassDumpTransformer.java
+++ core/src/main/java/com/taobao/arthas/core/command/klass100/ClassDumpTransformer.java
@@ -1,10 +1,5 @@
 package com.taobao.arthas.core.command.klass100;
 
-import com.alibaba.arthas.deps.org.slf4j.Logger;
-import com.alibaba.arthas.deps.org.slf4j.LoggerFactory;
-import com.taobao.arthas.core.util.FileUtils;
-import com.taobao.arthas.core.util.LogUtil;
-
 import java.io.File;
 import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
@@ -13,6 +8,11 @@ import java.security.ProtectionDomain;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
+import com.alibaba.arthas.deps.org.slf4j.Logger;
+import com.alibaba.arthas.deps.org.slf4j.LoggerFactory;
+import com.taobao.arthas.core.util.FileUtils;
+import com.taobao.arthas.core.util.LogUtil;
 
 /**
  * @author beiwei30 on 25/11/2016.
@@ -24,7 +24,7 @@ class ClassDumpTransformer implements ClassFileTransformer {
     private Set<Class<?>> classesToEnhance;
     private Map<Class<?>, File> dumpResult;
     private File arthasLogHome;
-
+    private String suffix = ".class";
     private File directory;
 
     public ClassDumpTransformer(Set<Class<?>> classesToEnhance) {
@@ -46,6 +46,13 @@ class ClassDumpTransformer implements ClassFileTransformer {
             dumpClassIfNecessary(classBeingRedefined, classfileBuffer);
         }
         return null;
+    }
+
+    public void setSuffix(String suffix) {
+        if (suffix == null) {
+            return;
+        }
+        this.suffix = suffix;
     }
 
     public Map<Class<?>, File> getDumpResult() {
@@ -72,9 +79,9 @@ class ClassDumpTransformer implements ClassFileTransformer {
         String fileName;
         if (classLoader != null) {
             fileName = classLoader.getClass().getName() + "-" + Integer.toHexString(classLoader.hashCode()) +
-                    File.separator + className.replace(".", File.separator) + ".class";
+                    File.separator + className.replace(".", File.separator) + suffix;
         } else {
-            fileName = className.replace(".", File.separator) + ".class";
+            fileName = className.replace(".", File.separator) + suffix;
         }
 
         File dumpClassFile = new File(dumpDir, fileName);
--- core/src/main/java/com/taobao/arthas/core/command/klass100/RedefineCommand.java
+++ core/src/main/java/com/taobao/arthas/core/command/klass100/RedefineCommand.java
@@ -5,13 +5,12 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.Instrumentation;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import com.alibaba.arthas.deps.org.slf4j.Logger;
 import com.alibaba.arthas.deps.org.slf4j.LoggerFactory;
+import com.taobao.arthas.core.advisor.Enhancer;
 import com.taobao.arthas.core.command.Constants;
 import com.taobao.arthas.core.shell.cli.Completion;
 import com.taobao.arthas.core.shell.cli.CompletionUtils;
@@ -29,9 +28,9 @@ import org.objectweb.asm.ClassReader;
 @Name("redefine")
 @Summary("Redefine classes. @see Instrumentation#redefineClasses(ClassDefinition...)|注意: Redefine后的类 reset/shutdown无法还原")
 @Description(Constants.EXAMPLE +
-                "  redefine /tmp/Test.class\n" +
-                "  redefine -c 327a647b /tmp/Test.class /tmp/Test\\$Inner.class \n" +
-                Constants.WIKI + Constants.WIKI_HOME + "redefine")
+        "  redefine /tmp/Test.class\n" +
+        "  redefine -c 327a647b /tmp/Test.class /tmp/Test\\$Inner.class \n" +
+        Constants.WIKI + Constants.WIKI_HOME + "redefine")
 public class RedefineCommand extends AnnotatedCommand {
     private static final Logger logger = LoggerFactory.getLogger(RedefineCommand.class);
     private static final int MAX_FILE_SIZE = 10 * 1024 * 1024;
@@ -39,6 +38,9 @@ public class RedefineCommand extends AnnotatedCommand {
     private String hashCode;
 
     private List<String> paths;
+
+    // 类-字节码缓存
+    protected final static Map<Class<?>/*Class*/, String /*bytes of Class*/> classBytesPathMap = new WeakHashMap<Class<?>, String>();
 
     @Option(shortName = "c", longName = "classloader")
     @Description("classLoader hashcode")
@@ -106,6 +108,7 @@ public class RedefineCommand extends AnnotatedCommand {
             return;
         }
 
+        Set<Class<?>> set = new HashSet<Class<?>>();
         List<ClassDefinition> definitions = new ArrayList<ClassDefinition>();
         for (Class<?> clazz : inst.getAllLoadedClasses()) {
             if (bytesMap.containsKey(clazz.getName())) {
@@ -113,14 +116,33 @@ public class RedefineCommand extends AnnotatedCommand {
                     continue;
                 }
                 definitions.add(new ClassDefinition(clazz, bytesMap.get(clazz.getName())));
+                set.add(clazz);
                 logger.info("Try redefine class name: {}, ClassLoader: {}", clazz.getName(), clazz.getClassLoader());
             }
         }
-
+        try {
+            //dump and backup class
+            process.write("backup start");
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+            ClassDumpTransformer transformer = new ClassDumpTransformer(set);
+            transformer.setSuffix(".class." + format.format(new Date()));
+            Enhancer.enhance(inst, transformer, set);
+            Map<Class<?>, File> backupClassMap = transformer.getDumpResult();
+            for (Map.Entry<Class<?>, File> entry : backupClassMap.entrySet()) {
+                process.write("bytecode backup class: " + entry.getKey().getName() + "\tpath:" + entry.getValue().getPath());
+                if (!classBytesPathMap.containsKey(entry.getKey().getName())) {
+                    classBytesPathMap.put(entry.getKey(), entry.getValue().getPath());
+                }
+            }
+            process.write("backup success");
+        } catch (Throwable t) {
+            process.write("backup error, size: " + t.getMessage());
+            logger.error("dump: fail to dump classes: ", t);
+        }
         try {
             if (definitions.isEmpty()) {
                 process.write("These classes are not found in the JVM and may not be loaded: " + bytesMap.keySet()
-                                + "\n");
+                        + "\n");
                 process.end();
                 return;
             }
